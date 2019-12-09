60522d537ebc847e03ab55e7a41461b62baeb080
Vladimir Shubarin <vhundef@gmail.com>
2019-12-09 10:52:02 +0300

    Lab 7 just another update

--- JS/Lab7/Lab7_2.js
+++ JS/Lab7/Lab7_2.js
@@ -2,7 +2,11 @@ var sum_d = 0;
 var sum_t = 0;
 var sum_c = 0;
 var sum_b = 0;
-
+function roundPlus(x,n) {
+    if(isNaN(x)||isNaN(n))return false;
+    var m= Math.pow(10,n);
+    return Math.round(x*m)/m
+}
 function tableChange() {
     var x = document.getElementById('furnChoose').value;
     if (document.getElementById('price').value !== '' && document.getElementById('quantity').value !== '') {
@@ -13,7 +17,7 @@ function tableChange() {
             var quantity_d = parseInt(document.getElementById('quantity').value);
             document.getElementById('deskQ').textContent = quantity_d;
 
-            sum_d = price_d * quantity_d;
+            sum_d = roundPlus(price_d * quantity_d,2);
             document.getElementById('deskRes').textContent = sum_d;
 
         } else if (x == 'table') {
@@ -23,7 +27,7 @@ function tableChange() {
             var quantity_t = parseInt(document.getElementById('quantity').value);
             document.getElementById('tableQ').textContent = quantity_t;
 
-            sum_t = price_t * quantity_t;
+            sum_t = roundPlus(price_t * quantity_t,2);
             document.getElementById('tableRes').textContent = sum_t;
 
         } else if (x == 'chair') {
@@ -33,7 +37,7 @@ function tableChange() {
             var quantity_c = parseInt(document.getElementById('quantity').value);
             document.getElementById('chairQ').textContent = quantity_c;
 
-            sum_c = price_c * quantity_c;
+            sum_c = roundPlus(price_c * quantity_c,2);
             document.getElementById('chairRes').textContent = sum_c;
 
         } else if (x == 'bookself') {
@@ -43,7 +47,7 @@ function tableChange() {
             var quantity_b = parseInt(document.getElementById('quantity').value);
             document.getElementById('bookselfQ').textContent = quantity_b;
 
-            sum_b = price_b * quantity_b;
+            sum_b = roundPlus(price_b * quantity_b,2);
             document.getElementById('bookselfRes').textContent = sum_b;
         }
         document.getElementById('sum').textContent = sum_d + sum_t + sum_c + sum_b;
