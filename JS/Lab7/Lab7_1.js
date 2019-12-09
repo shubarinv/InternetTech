19b027aee690e949f6352f1967c860b0483b31e0
Vladimir Shubarin <vhundef@gmail.com>
2019-12-09 10:17:03 +0300

    Updated 7.1 so that it works better

--- JS/Lab7/Lab7_1.js
+++ JS/Lab7/Lab7_1.js
@@ -7,55 +7,55 @@ function check() {
     angleDeg_st = parseInt(document.getElementById('st_val').value);
     angleDeg_end = parseInt(document.getElementById('end_val').value);
     angleStep = parseInt(document.getElementById('step').value);
+    if (angleDeg_st < angleDeg_end||angleStep==='') {
+        var i = angleDeg_st;
+        var winObj = window.open("Lab7_1_2.html");
+        winObj.document.write
+        ('<table border="1">' +
+            '<tr>' +
+            '<td colspan="2">Угол</td>' +
+            '<td rowspan="2" id="funcArea"></td>' +
+            '</tr>' +
+            '<tr>' +
+            '<td>в градусах</td>' +
+            '<td>в радианах</td>' +
+            '/tr');
 
-    var i = angleDeg_st;
-    var winObj = window.open("Lab7_1_2.html");
+        while (i <= angleDeg_end) {
+            angleRad = i * Math.PI / 180;
 
-    winObj.document.write
-    ('<table border="1">' +
-        '<tr>' +
-        '<td colspan="2">Угол</td>' +
-        '<td rowspan="2" id="funcArea"></td>' +
-        '</tr>' +
-        '<tr>' +
-        '<td>в градусах</td>' +
-        '<td>в радианах</td>' +
-        '/tr');
+            if (valueFunc == 'sin') {
+                winObj.document.getElementById('funcArea').textContent = "sin";
 
-    while (i <= angleDeg_end) {
-        angleRad = i * Math.PI / 180;
+                winObj.document.write
+                ('<tr>' +
+                    '<td>' + i + '</td>' +
+                    '<td>' + angleRad.toFixed(3) + '</td>' +
+                    '<td>' + Math.sin(angleRad).toFixed(3) + '</td>' +
+                    '</tr>');
 
-        if (valueFunc == 'sin') {
-            winObj.document.getElementById('funcArea').textContent = "sin";
+            } else if (valueFunc == 'cos') {
+                winObj.document.getElementById('funcArea').textContent = "cos";
 
-            winObj.document.write
-            ('<tr>' +
-                '<td>' + i + '</td>' +
-                '<td>' + angleRad + '</td>' +
-                '<td>' + Math.sin(angleRad) + '</td>' +
-                '</tr>');
+                winObj.document.write
+                ('<tr>' +
+                    '<td>' + i + '</td>' +
+                    '<td>' + angleRad.toFixed(3) + '</td>' +
+                    '<td>' + Math.cos(angleRad).toFixed(3) + '</td>' +
+                    '</tr>');
 
-        } else if (valueFunc == 'cos') {
-            winObj.document.getElementById('funcArea').textContent = "cos";
+            } else if (valueFunc == 'tan') {
+                winObj.document.getElementById('funcArea').textContent = "tan";
 
-            winObj.document.write
-            ('<tr>' +
-                '<td>' + i + '</td>' +
-                '<td>' + angleRad + '</td>' +
-                '<td>' + Math.cos(angleRad) + '</td>' +
-                '</tr>');
+                winObj.document.write
+                ('<tr>' +
+                    '<td>' + i + '</td>' +
+                    '<td>' + angleRad.toFixed(3) + '</td>' +
+                    '<td>' + Math.tan(angleRad).toFixed(3) + '</td>' +
+                    '</tr>');
+            }
 
-        } else if (valueFunc == 'tan') {
-            winObj.document.getElementById('funcArea').textContent = "tan";
-
-            winObj.document.write
-            ('<tr>' +
-                '<td>' + i + '</td>' +
-                '<td>' + angleRad + '</td>' +
-                '<td>' + Math.tan(angleRad) + '</td>' +
-                '</tr>');
+            i += angleStep;
         }
-
-        i += angleStep;
     }
 }
