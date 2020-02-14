a88218332d695a0d2cd2206d8df4627909206da2
Vladimir Shubarin <vhundef@gmail.com>
2020-02-14 20:26:37 +0300

    Internet Tech global upd

--- JS/Lab3/Lab3_1.php
+++
@@ -1,9 +0,0 @@
-<?php
-$furniture = $_POST["furniture"];
-$materials = $_POST["materials"];
-$quantity = $_POST["quantity"];
-echo "Your order is ready!";
-echo "Furniture - $furniture";
-echo "Material - $materials";
-echo "Quantity - $quantity";
-?>
